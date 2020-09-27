590cbef0ed8a14e8b8f36ce5fc6cd1f428d7ca2d
Sukoon Sarin <sukoonsarin@gmail.com>
2020-09-27 10:18:37 -0400

    Setters and getters implemented for gain variables

--- include/pid.hpp
+++ include/pid.hpp
@@ -3,11 +3,30 @@
 namespace tdd {
     class PIDController{
         private:
-            double kp, ki, kd;
+            double kp_, ki_, kd_;
             double target_velocity;
             double current_velocity;
             double error_threshold;
         public:
+            //Constructer
+            PIDController(double kp, double ki, double kd) {
+                kp_ = kp;
+                ki_ = ki;
+                kd_ = kd;
+            }
+            
+            //Destructor 
+            ~PIDController();
+
+            //Settes
+
+            void setKp(double);
+            void setKi(double);
+            void setKd(double);
+            double getKp();
+            double getKi();
+            double getKd();
+
             double computeError();
             double computeVelocity();
                     
