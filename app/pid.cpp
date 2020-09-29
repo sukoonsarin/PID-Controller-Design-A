52c7656ce64720931dc6783936528cd96a9d16f3
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-09-29 00:19:26 -0400

    Added comments and updated PID output defn.

--- app/pid.cpp
+++ app/pid.cpp
@@ -4,18 +4,19 @@
 
 double tdd::PIDController::compute(double target_vel, double current_vel)
 {   
-    target_velocity = target_vel;
-    current_velocity = current_vel;
-    double dt = 0.1;
-    double currError = 0;
-    double prevError = 0;
-    double integError = 0;
-    double errorThreshold = 0.1;
-    std::cout << std::abs(23.3) << std::endl;
-    while(std::abs(target_velocity - current_vel) >= std::abs(errorThreshold)){
-        currError = target_velocity - current_vel;
-        integError+= currError*dt;
-        current_vel = kp_*currError + ki_*integError + (kd_*(currError - prevError))/dt;
+    target_velocity = target_vel; // Set class variable target_velocity to user set target_vel
+    current_velocity = current_vel; // Set class variable current_velocity to user set current_vel
+    double dt = 0.1; // set default value dt = 0.1
+    double currError = 0; // set default to 0
+    double prevError = 0; // set default to 0
+    double integError = 0; // set default to 0
+    double errorThreshold = 0.1; // set default to 0.1 
+    double controlOutput = 0.0; // set defualt to 0
+    while(std::abs(target_velocity - current_vel) >= std::abs(errorThreshold)){ // While error still greater than threshold.
+        currError = target_velocity - current_vel; // calc currError
+        integError+= currError*dt; // update integral error
+        controlOutput = kp_*currError + ki_*integError + (kd_*(currError - prevError))/dt; // Calc PID Control output 
+        current_vel += controlOutput; // Update current velocity 
         prevError = currError;
     }
     return current_vel;
