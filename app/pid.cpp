8d3f825c10c058ead2b114710dbce7b5517a2d4b
vishnuu95 <vishnuu@terpmail.umd.edu>
2020-09-29 11:24:35 -0400

    Added initialisations and variables

--- app/pid.cpp
+++ app/pid.cpp
@@ -23,6 +23,14 @@ double tdd::PIDController::compute(double target_velocity,
                                     double current_velocity) {
     // Enter your PID compute method implemetation
     return 66;
+    target_velocity = target_vel; // Set class variable target_velocity to user set target_vel
+    current_velocity = current_vel; // Set class variable current_velocity to user set current_vel
+    double dt = 0.1; // set default value dt = 0.1
+    double currError = 0; // set default to 0
+    double prevError = 0; // set default to 0
+    double integError = 0; // set default to 0
+    double errorThreshold = 0.1; // set default to 0.1 
+    double controlOutput = 0.0; // set defualt to 0
 }
 
 void tdd::PIDController::setKp(double kp) {
