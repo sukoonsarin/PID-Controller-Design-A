e9538a0667f0369ff43db368944762f28bea78cc
sukoonsarin <54259959+sukoonsarin@users.noreply.github.com>
2020-09-28 21:16:51 -0400

    Update test.cpp with comments

--- test/test.cpp
+++ test/test.cpp
@@ -15,6 +15,9 @@
 #include <gtest/gtest.h>
 #include "../include/pid.hpp"
 
+/**
+ * Test class initialization
+ */
 TEST(PIDControllerTest, classInitialization) {
   tdd::PIDController dummyPIDController(0.2, 0.4, 0.02);
   EXPECT_EQ(0.2, dummyPIDController.getKp());
@@ -22,6 +25,9 @@ TEST(PIDControllerTest, classInitialization) {
   EXPECT_EQ(0.02, dummyPIDController.getKd());
 }
 
+/**
+ * Test compute method
+ */
 TEST(PIDControllerTest, computeNewVelocity) {
   tdd::PIDController dummyPIDController(0.2, 0.4, 0.02);
   double target_velocity = 70, current_velocity = 30;
@@ -29,6 +35,9 @@ TEST(PIDControllerTest, computeNewVelocity) {
                                             current_velocity), 2);
 }
 
+/**
+ * Test Individual PID parameter (Kp, kd, ki) setter routines
+ */
 TEST(PIDControllerTest, setParamters) {
   tdd::PIDController dummyPIDController(0.2, 0.4, 0.02);
   dummyPIDController.setKp(0.7);
@@ -39,6 +48,9 @@ TEST(PIDControllerTest, setParamters) {
   EXPECT_EQ(0.06, dummyPIDController.getKd());
 }
 
+/**
+ * Test Individual PID parameter (Kp, kd, ki) getter routines
+ */
 TEST(PIDControllerTest, getParamters) {
   tdd::PIDController dummyPIDController(0.5, 0.3, 0.2);
 
